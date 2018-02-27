8cea3eefb9dfdb8252237493826cfc0f1f7025d2
Michel <michel.bolzon123@gmail.com>
2018-02-26 23:13:35 -0300

    email

--- config/config.php
+++ config/config.php
@@ -53,13 +53,13 @@ $config['site']['max_players_per_account'] = 15;
 
 # Emails Config
 $config['site']['send_emails'] = true;
-$config['site']['mail_address'] = "xxx@xxxx.com.br";
+$config['site']['mail_address'] = "baiak-completo@gmail.com";
 $config['site']['smtp_enabled'] = true;
-$config['site']['smtp_host'] = "smtp.xxxxx.com.br";
+$config['site']['smtp_host'] = "smtp.gmail.com";
 $config['site']['smtp_port'] = 587;
 $config['site']['smtp_auth'] = true;
-$config['site']['smtp_user'] = "xxx@xxx.com.br";
-$config['site']['smtp_pass'] = "xxx";
+$config['site']['smtp_user'] = "baiak-completo@gmail.com";
+$config['site']['smtp_pass'] = "Baiakcomplet!";
 
 # PAGE: whoisonline.php
 $config['site']['private-servlist.com_server_id'] = 1;
